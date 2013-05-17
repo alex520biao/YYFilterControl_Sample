603fd8458306989c31888ae0af98a4a2f610c1de
alex520biao <62120>
2013-05-17 22:54:59 +0800

    修改xib的问题

--- YYFilterControl_Sample/AppDelegate.m
+++ YYFilterControl_Sample/AppDelegate.m
@@ -26,7 +26,7 @@
     // Override point for customization after application launch.
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
     } 
-    self.viewController = [[[ViewController alloc] initWithNibName:@"viewController" bundle:nil] autorelease];
+    self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController" bundle:nil] autorelease];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
